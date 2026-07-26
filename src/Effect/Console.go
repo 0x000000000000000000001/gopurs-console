ff73101243232cd7f435f641c218632312156b97
0x1 <kevin.francart3@gmail.com>
2026-07-26 16:08:32 +0200

    Auto-commit before architecture refactoring

--- src/Effect/Console.go
+++ src/Effect/Console.go
@@ -4,58 +4,58 @@ import (
 	"fmt"
 )
 
-func Log(s string) func() any {
-	return func() any {
+func Log(s string) func() interface{} {
+	return func() interface{} {
 		fmt.Println(s)
 		return nil
 	}
 }
 
-func Warn(s string) func() any {
-	return func() any {
+func Warn(s string) func() interface{} {
+	return func() interface{} {
 		fmt.Println("[WARN]", s)
 		return nil
 	}
 }
 
-func Error(s string) func() any {
-	return func() any {
+func Error(s string) func() interface{} {
+	return func() interface{} {
 		fmt.Println("[ERROR]", s)
 		return nil
 	}
 }
 
-func Info(s string) func() any {
-	return func() any {
+func Info(s string) func() interface{} {
+	return func() interface{} {
 		fmt.Println("[INFO]", s)
 		return nil
 	}
 }
 
-func Debug(s string) func() any {
+func Debug(s string) func() interface{} {
 	return Log(s)
 }
 
-func Time(s string) func() any {
+func Time(s string) func() interface{} {
 	return Log(s)
 }
 
-func TimeLog(s string) func() any {
+func TimeLog(s string) func() interface{} {
 	return Log(s)
 }
 
-func TimeEnd(s string) func() any {
+func TimeEnd(s string) func() interface{} {
 	return Log(s)
 }
 
 func Clear() {
 }
 
-func Group(s string) func() any {
+func Group(s string) func() interface{} {
 	return Log(s)
 }
 
-func GroupCollapsed(s string) func() any {
+func GroupCollapsed(s string) func() interface{} {
 	return Log(s)
 }
 
