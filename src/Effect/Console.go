a8f27176df0b46793b499fe16deaec0710f7626b
0x1 <kevin.francart3@gmail.com>
2026-08-01 13:49:46 +0200

    fix(ffi): fix various FFI implementations

--- src/Effect/Console.go
+++ src/Effect/Console.go
@@ -1,69 +1,57 @@
-
+package Effect_Console
 
 import (
 	"fmt"
 )
 
-func Log(s string) func() interface{} {
-	return func() interface{} {
-		fmt.Println(s)
-		return nil
-	}
+func Log(s string, _ interface{}) interface{} {
+	fmt.Println(s)
+	return nil
 }
 
-func Warn(s string) func() interface{} {
-	return func() interface{} {
-		fmt.Println("[WARN]", s)
-		return nil
-	}
+func Warn(s string, _ interface{}) interface{} {
+	fmt.Println("[WARN]", s)
+	return nil
 }
 
-func Error(s string) func() interface{} {
-	return func() interface{} {
-		fmt.Println("[ERROR]", s)
-		return nil
-	}
+func Error(s string, _ interface{}) interface{} {
+	fmt.Println("[ERROR]", s)
+	return nil
 }
 
-func Info(s string) func() interface{} {
-	return func() interface{} {
-		fmt.Println("[INFO]", s)
-		return nil
-	}
+func Info(s string, _ interface{}) interface{} {
+	fmt.Println("[INFO]", s)
+	return nil
 }
 
-func Debug(s string) func() interface{} {
-	return Log(s)
+func Debug(s string, _ interface{}) interface{} {
+	return Log(s, nil)
 }
 
-func Time(s string) func() interface{} {
-	return Log(s)
+func Time(s string, _ interface{}) interface{} {
+	return Log(s, nil)
 }
 
-func TimeLog(s string) func() interface{} {
-	return Log(s)
+func TimeLog(s string, _ interface{}) interface{} {
+	return Log(s, nil)
 }
 
-func TimeEnd(s string) func() interface{} {
-	return Log(s)
+func TimeEnd(s string, _ interface{}) interface{} {
+	return Log(s, nil)
 }
 
-func Clear() func() interface{} {
-	return func() interface{} {
-		return nil
-	}
+func Clear(_ interface{}) interface{} {
+	return nil
 }
 
-func Group(s string) func() interface{} {
-	return Log(s)
+func Group(s string, _ interface{}) interface{} {
+	return Log(s, nil)
 }
 
-func GroupCollapsed(s string) func() interface{} {
-	return Log(s)
+func GroupCollapsed(s string, _ interface{}) interface{} {
+	return Log(s, nil)
 }
 
-func GroupEnd() func() interface{} {
-	return func() interface{} {
-		return nil
-	}
+func GroupEnd(_ interface{}) interface{} {
+	return nil
 }
